bb727ecbb927960ef4ad4690e0f0a04a5c3e16fa
IsmailTahmout <150095962+IsmailTahmout@users.noreply.github.com>
2026-02-24 05:36:43 +0000

    Refactor About.jsx for responsive design issues

---
+++ About.jsx
@@ -0,0 +1,16 @@
+// Updated About.jsx
+
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">About Us</h1>
+            <p className="responsive-text">
+                We are committed to delivering the best services to our clients.
+            </p>
+        </div>
+    );
+};
+
+export default About;
